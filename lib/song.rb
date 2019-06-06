0addad9752111575ffb956b81eccfc6ee0414384
Philip Sterling <philipssterlings@gmail.com>
2019-06-05 17:39:50 -0700

    still confused

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song 
+    attr_accessor :name, :artist
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self 
+    end
+    def artist_name 
+        if self.artist
+            self.artist.name
+        else
+            return nil
+        end
+    end
+    def self.all 
+        @@all
+    end
+end
